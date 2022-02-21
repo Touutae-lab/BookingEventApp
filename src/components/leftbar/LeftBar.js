6d5c578a46e75ee0d911acb8febd4d68f67cc741
cscmu2021 <98556429+cscmu2021@users.noreply.github.com>
2022-02-21 13:21:55 +0700

    divide components

--- src/components/leftbar/LeftBar.js
+++ src/components/leftbar/LeftBar.js
@@ -3,11 +3,10 @@ import "./LeftBar.css";
 import axios from "axios";
 import React from 'react';
 
-import Avatar from "@mui/material/Avatar";
-import { deepOrange, orange, red, pink, blue, green } from "@mui/material/colors";
+
 import ShowDate from "./ShowDate";
 import ShowLogin from "./ShowLogin";
-import FormLogin from "./FormLogin";
+import ShowUser from "./ShowUser";
 
 class LeftBar extends React.Component {
   constructor(props) {
@@ -27,59 +26,9 @@ class LeftBar extends React.Component {
     // this.handleChange = this.handleChange.bind(this);
   }
 
-  handleClick = () => {
-    this.state.open = !this.state.open;
-  };
 
-  handleSubmit(e) {
-    e.preventDefault();
-    axios.post("http://ec2-13-229-129-189.ap-southeast1.compute.amazonaws.com/users", {
-      userId: "234567890-",
-      displayName: "ja",
-      username: "jarawin",
-      email: "jarawin@gmail.com",
-      role_id: "2"
-    })
-      .then(response => this.setState({ token: response }))
-  }
 
-  componentDidMount() {
-    if (this.state.token.status === "200") {
-      this.state.page = "loggedin"
-    }
-  }
 
-  componentDidUpdate() {
-    console.log("LeftBar componentDidUpdate")
-    if (this.state.userData !== null) {
-      console.log(this.state)
-    }
-  }
-
-  handleChange(e) {
-    const value = e.target.value;
-    this.setState({
-      [e.target.name]: value
-    })
-  }
-
-  // multi page
-  pageLoggedin() {
-    return (
-      <div className="mt-3">
-        <h1>{this.state.day}</h1>
-        <h4>{this.state.date}</h4>
-        <br />
-
-        <Avatar sx={{ bgcolor: deepOrange[500], width: 56, height: 56 }} className="img-center">
-          {this.state.name[0]}
-        </Avatar>
-
-        <h2>{this.state.name}</h2>
-        <p>Your Team</p>
-      </div>
-    );
-  }
 
   setUserData(userData) {
     this.setState({
@@ -100,8 +49,8 @@ class LeftBar extends React.Component {
           <ShowLogin setUserData={this.setUserData} />
         </div>
 
-        <div className="mt-3">
-          <FormLogin setUserData={this.setUserData} />
+        <div>
+          <ShowUser name="ja" />
         </div>
       </div>
     )
--- src/components/leftbar/ShowLogin.js
+++ src/components/leftbar/ShowLogin.js
@@ -2,62 +2,87 @@ import "./ShowLogin.css";
 import React from 'react';
 import axios from "axios";
 
-class ShowLogin extends React.Component {
+class FormLogin extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            userName: "",
-            passWord: "",
-            userData: props.userData,
-        };
-        this.handleChange = this.handleChange.bind(this);
+            email: "",
+            password: "",
+        }
     }
 
-    componentDidMount() {
-        console.log(this.state.userData)
-        console.log(this.state.userData)
-        // this.forceUpdate()
-    }
-
-    handleChange(e) {
+    changeHandler = e => {
         const name = e.target.name;
         const value = e.target.value;
 
-        this.setState({ [name]: value });
+        this.setState({
+            ...this.state,
+            [name]: value
+        });
+
+        console.log(this.state.email);
+        console.log(this.state.password);
     }
 
-    
+    handleSubmit = e => {
+        e.preventDefault();
+
+        if (this.state.email === "" || this.state.password === "") {
+            alert("Please enter your username and password")
+        } else {
+            console.log("submit");
+            console.log(this.state.email);
+            console.log(this.state.password);
+            try {
+                axios.get("http://ec2-13-229-129-189.ap-southeast-1.compute.amazonaws.com/login", {
+                    email: this.state.email,
+                    hashpassword: this.setState.password,
+                })
+                    .then((res) => {
+                        console.log("response")
+                        console.log(res.data)
+
+                        if (res.data.status === "200") {
+                            this.props.setUserData(res.userdata)
+                        } else {
+                            alert(res.message)
+                        }
+                    })
+
+            } catch (e) {
+                alert("error")
+            }
+        }
+    }
+
 
     render() {
         return (
             <div className="text-center">
                 <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label>Username</label>
-                        <input
-                            className="mb-05"
-                            type="text"
-                            value={this.state.userName}
-                            name="userName"
-                            onChange={this.onChange}
-                        />
-
-                        <label >Password</label>
-
-                        <input
-                            className="mb-1"
-                            type="text"
-                            name="passWord"
-                            value={this.state.passWord}
-                            onChange={this.onChange}
-                        />
-
-                        <input type="submit" />
-                    </div>
+                    <label>email</label>
+                    <input type="email"
+                        className="mb-05"
+                        placeholder="exameple@gmail.com"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.changeHandler}
+                    />
+                    <label>password</label>
+                    <input type="password"
+                        className="mb-1"
+                        placeholder="12345678"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.changeHandler}
+                    />
+                    <button className="mb-3" type="submit">Login</button>
                 </form>
             </div>
-        )
+        );
     }
+
 }
 
-export default ShowLogin;
+
+export default FormLogin;
